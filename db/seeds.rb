ed18237208dc10e5250830118d7ad10d6618dbc9
Chris Goodwin <chris_2190@hotmail.com>
2019-08-21 15:15:16 +1000

    MVP Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = %w[chinese italian japanese french belgian]
+new_restaurants = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '12622431',
+    category:     categories.sample
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12622432',
+    category:      categories.sample
+  },
+    {
+    name:         'Sushi Samba',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '12622433',
+    category:      categories.sample
+  },
+  {
+    name:         'Carretao',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12622434',
+    category:      categories.sample
+  },
+    {
+    name:         'Lala Land',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '12622435',
+    category:      categories.sample
+  }
+]
+
+Restaurant.create!(new_restaurants)
